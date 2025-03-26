a2e6e3442af6b4a3733d5134e06169090103bf84
George Mousa <67595912+gmousa@users.noreply.github.com>
2025-03-25 22:48:12 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -143,6 +143,9 @@ def create_html_content(table_html, title):
     <script type="text/javascript" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
     <script type="text/javascript" src="https://cdn.datatables.net/searchbuilder/1.4.2/js/dataTables.searchBuilder.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.13.4/sorting/datetime-moment.js"></script>
+
 
     <!-- Add DataTables CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css">
@@ -225,15 +228,18 @@ def create_html_content(table_html, title):
     }}
     </style>
 
-       <script>
+    <script>
     $(document).ready(function() {{
+        // Set the date format for parsing
+        $.fn.dataTable.moment('M/D/YYYY');
+        
         var table = $('#myTable').DataTable({{
             scrollX: true,
             scrollCollapse: true,
             paging: false,
             responsive: false,
             autoWidth: false,
-            order: [[0, 'asc']],
+            order: [[1, 'asc']], // Sort by date column (index 1)
             dom: 'Brti',
             buttons: [
                 'copy', 'csv', 'excel', 'pdf', 'print'
@@ -242,6 +248,11 @@ def create_html_content(table_html, title):
                 orderable: false,
                 targets: '_all',
                 width: 'auto'
+            }},
+            {{
+                targets: 1,
+                orderable: true,
+                type: 'date'
             }}]
         }});
 
